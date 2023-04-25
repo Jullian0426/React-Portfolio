712baaf34a878bb9a451c465b10e5133a6d56906
Jullian0426 <jullian0426@yahoo.com>
2023-04-25 02:27:51 -0500

    Create project cards and map through them with props

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Project from './Project';
+import ProjectCard from './Project';
+import portfolio from '../portfolio.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Portfolio() {
@@ -7,6 +8,12 @@ function Portfolio() {
         <section>
             <div className='portfolio'>
                 <h1 className='h1'>My Portfolio</h1>
+            </div>
+
+            <div id='project'>
+                {portfolio.map((project) => {
+                   return <ProjectCard key={project.id} image={project.image} name={project.title} github={project.github} deploy={project.deploy}/>
+                })}
             </div>
         </section>
     )
---
+++ src/components/Project.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ProjectCard(props) {
+    return (
+        <div className="card">
+            <img class="card-img-top" src={props.image} alt={props.title}/>
+                <div class="card-body">
+                    <h5 class="card-title">{props.title}</h5>
+                    <a href={props.github} class="btn btn-primary">GitHub</a>
+                    <a href={props.deploy} class="btn btn-primary">Deployed App</a>
+                </div>
+        </div>
+    )
+};
+
+export default ProjectCard;
